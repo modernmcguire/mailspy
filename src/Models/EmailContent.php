5e654c899d1fb211653459f77c804ba4c6a943ce
modernben <67608755+modernben@users.noreply.github.com>
2025-02-25 16:00:00 +0000

    Fix styling

--- src/Models/EmailContent.php
+++ src/Models/EmailContent.php
@@ -2,8 +2,8 @@
 
 namespace ModernMcGuire\MailSpy\Models;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Model;
 
 class EmailContent extends Model
 {
